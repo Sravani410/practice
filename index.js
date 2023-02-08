0a837319c730e16a500de824d47d34afbaf35287
Aniruddh Agarwal <aniruddh.a@neosoftmail.com>
2023-02-08 11:24:58 +0530

    structure

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-console.log("google auth");
-//   <div className="App">
-{/* <h1>Authentication</h1>
-<form>
-  <input type="email" name="name" placeholder="Email"/>
-  <br/>
-  <input type="password" name="password" placeholder="Password"/><br/>
-  <button>Sign In</button> &nbsp; &nbsp;
-  <button>Sign Up</button>
-</form>
-</div> */}
+console.log("phone number auth");
---
+++ phone-auth/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BsFillShieldLockFill } from "react-icons/bs";
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+        <h1>Welcome to Phone Auth</h1>
+        <div>
+          <BsFillShieldLockFill />
+        </div>
+      </center>
+    </div>
+  );
+}
+
+export default App;
