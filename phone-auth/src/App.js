72b406de51301823fc47141886040ce1c7a3b555
Aniruddh Agarwal <aniruddh.a@neosoftmail.com>
2023-02-14 10:33:04 +0530

    change

--- phone-auth/src/App.js
+++ phone-auth/src/App.js
@@ -8,20 +8,8 @@ function App() {
      <PhoneAuth/> 
     </>
 
-import { BsFillShieldLockFill } from "react-icons/bs";
 
-function App() {
-  return (
-    <div className="App">
-      <center>
-        <h1>Welcome to Phone Auth</h1>
-        <div>
-          <BsFillShieldLockFill />
-        </div>
-      </center>
-    </div>
 
-  );
+  )
 }
-
 export default App;
